5cd0036cb8cb97d8ef2aece02cc60a4b0cfbcefc
ezheniko <ezh.eny.kozhevik@gmail.com>
2018-02-06 01:35:36 +0200

    fix nav animation

--- script.js
+++ script.js
@@ -3,9 +3,27 @@ document.addEventListener("DOMContentLoaded", init);
 function init() {
   const navigationElements = document.querySelectorAll(".navigation__elem");
   let num = 1;
+
+  let startRightCord = document.querySelector(".sidebar").getBoundingClientRect().right;
+
   for (let navElem of navigationElements) {
-    setTimeout(() => {
-      navElem.style.marginLeft = 0;
-    }, 500 * num++);
+    navElem.style.right = startRightCord;
+    navElem.style.transition = ".5s";
   }
+
+  if (window.innerWidth > 768) {
+    for (let navElem of navigationElements) {
+      setTimeout(() => {
+        navElem.style.right = 0;
+      }, 1 * num);
+      num += 250;
+    }
+  } else {
+    for (let i = navigationElements.length - 1; i >= 0; i--, num += 250) {
+      setTimeout(() => {
+        navigationElements[i].style.right = 0;
+      }, 1 * num);
+    }
+  }
+
 }
